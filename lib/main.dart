3ba3fb1f57427a67483d5334a58d8cdfb55416cd
MuyoNz <pattaraponler@gmail.com>
2021-05-20 17:45:57 +0700

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:thailand_covid_19/screens/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: Scaffold(),
+      initialRoute: "/home",
+      getPages: [
+        GetPage(name: "/home", page: () => HomeScreen()),
+      ],
     );
   }
 }
